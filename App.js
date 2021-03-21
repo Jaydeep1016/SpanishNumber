87240ac2184f76173d9bd61acccc26716fc270c3
Jaydeep1016 <jaydeepvaghela1016jp.jv@gmail.com>
2021-03-21 22:50:56 +0530

    Startup App with Assets and print on display SpanishNumber

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Spanish Number</Text>
+    </>
+  );
+};
+
+export default App;
